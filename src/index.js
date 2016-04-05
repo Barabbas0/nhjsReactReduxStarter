f8e46016d7b166c452626315196754bcd661f790
estaub <ed.staub@semanterra.org>
2016-04-05 15:07:55 -0400

    Add hot module reload, source map to build, Redux dev tools

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { Router, browserHistory } from 'react-router';
 
-import App from './components/app';
 import reducers from './reducers';
+import routes from './routes';
 
 // If running with Redux Dev Tools, let them wrap the store
 const createStore2 = window.devToolsExtension
@@ -15,6 +16,8 @@ const createStoreWithMiddleware = applyMiddleware()(createStore2);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory} routes={routes} >
+
+    </Router>
   </Provider>
   , document.querySelector('.container'));
